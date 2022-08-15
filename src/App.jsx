fc657925a8ff97023f91776bedf1de9460f3e0b7
Kalp Patel <kalp_patel@rocketmail.com>
2022-08-15 10:46:37 +0530

    imported all components using index.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import React from "react";
 import styles from "./styles";
+
+import {
+  NavBar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonial,
+  Client,
+  CTA,
+  Footer,
+} from "./components";
+
 const App = () => {
   return (
     <div className="bg-primary w-full overflow-hidden">
@@ -11,9 +25,7 @@ const App = () => {
         <div className={`${styles.boxWidth}`}>Hero</div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
-          stats business billing cardDeal Testimonials client cta footer
-        </div>
+        <div className={`${styles.boxWidth}`}></div>
       </div>
     </div>
   );
