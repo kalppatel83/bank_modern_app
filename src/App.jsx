5e89db26ce7736ba0253eb8695c72e61fb8fb5ad
Kalp Patel <kalp_patel@rocketmail.com>
2022-08-15 10:17:26 +0530

    app.js basic base updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import styles from "./styles";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>NavBar</div>
+      </div>
+      {/* Hero */}
+      <div className={`bg-primary | ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          stats business billing cardDeal Testimonials client cta footer
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
